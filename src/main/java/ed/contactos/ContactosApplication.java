7458211954c05529c39e5d3f3a7e8404385ee101
Enrique <enriquecalar@gmail.com>
2025-07-14 12:08:35 +0200

    Sistema de Contactos

--- src/main/java/ed/contactos/ContactosApplication.java
+++ src/main/java/ed/contactos/ContactosApplication.java
@@ -12,5 +12,4 @@ public class ContactosApplication {
 
 		SpringApplication.run(ContactosApplication.class, args);
 	}
-
 }
--- src/main/java/ed/contactos/controlador/ContactoControlador.java
+++ src/main/java/ed/contactos/controlador/ContactoControlador.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +25,40 @@ public class ContactoControlador {
         modelo.put("contactos", contactos);
         return "index"; //index.html
     }
+
+    @GetMapping("/agregar")
+    public String mostrarAgregar(){
+        return "agregar";//Agregar.html
+    }
+
+    @PostMapping("/agregar")
+    public String agregar(@ModelAttribute("contactoForm") Contacto contacto){
+        logger.info("Contacto a agregar: " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";//Redirigimos al controlador el path"/"
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEditar(@PathVariable(value = "id") int idContacto, ModelMap modelo){
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Contacto a editar (mostrar): " + contacto);
+        modelo.put("contacto", contacto);
+        return "editar"; //editar.html
+    }
+
+    @PostMapping("/editar")
+    public String editar(@ModelAttribute("contacto") Contacto contacto){
+        logger.info("Contacto a guardar: " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";//redirigir a controlador path "/"
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value = "id") int idContacto){
+        Contacto contacto = new Contacto();
+        contacto.setIdContacto(idContacto);
+        contactoServicio.eliminarContacto(contacto);
+        return "redirect:/";
+    }
+
 }
